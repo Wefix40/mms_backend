3154f6554c9a392e3667f9223e5c3e1d4c5b65ad
ajay <Wefix40@gmail.com>
2026-08-31 13:27:51 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,9 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
     ObserveModule.forRoot({
-      appKey: 'BYuZE^Jf9xItEZPO',
-      appSecret: 'y7ow3TNxVV73LbGj0JHzTid!jz!Rl&YgKKebYB4Ujm^2l',
-      serviceId: 'MMS',
+      appKey: 'YOUR_APP_KEY',
+      appSecret: 'YOUR_APP_SECRET',
+      serviceId: 'mms_backend',
     }),
   ],
   controllers: [AppController],
